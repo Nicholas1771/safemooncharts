a6adb994a55780c6a339bd58fa3c7df22ce2e8b6
nicho <nicho@10.0.0.227>
2021-07-21 15:07:58 -0400

    Initial commit

---
+++ safemoonchartsapp/src/main/java/com/safemooncharts/safemoonchartsapp/SafemoonchartsappApplication.java
@@ -0,0 +1,13 @@
+package com.safemooncharts.safemoonchartsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafemoonchartsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafemoonchartsappApplication.class, args);
+	}
+
+}
---
+++ safemoonchartsapp/src/test/java/com/safemooncharts/safemoonchartsapp/SafemoonchartsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safemooncharts.safemoonchartsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafemoonchartsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
