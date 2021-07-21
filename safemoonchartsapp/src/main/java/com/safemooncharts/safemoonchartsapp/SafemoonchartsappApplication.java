1b212708e12ebf255231738a58b173caa91bce3f
nicho <nicho@10.0.0.227>
2021-07-21 15:28:05 -0400

    commit 3

--- safemoonchartsapp/src/main/java/com/safemooncharts/safemoonchartsapp/SafemoonchartsappApplication.java
+++ safemoonchartsapp/src/main/java/com/safemooncharts/safemoonchartsapp/SafemoonchartsappApplication.java
@@ -2,9 +2,10 @@ package com.safemooncharts.safemoonchartsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SafemoonchartsappApplication {
+public class SafemoonchartsappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafemoonchartsappApplication.class, args);
